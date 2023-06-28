c9092c51b9dff8c24758446858b07aeb61b03147
Bruno Oliveira <109918729+bruno-deoliveira@users.noreply.github.com>
2023-06-27 21:25:39 -0300

    Add files via upload

---
+++ Perguntando-nome-e-mostrando-mensagem-03/script.js
@@ -0,0 +1,14 @@
+//Perguntando nome e mostrando mensagem
+
+alert("Agora eu sei programar em js");
+//Encontre a solução ao problema:
+"Pergunte o nome do usuário e escreva a mensagem:"
+//Olá, [nome do usuário]"
+
+let nome = prompt("Qual o seu nome?");
+alert ("Olá " + nome);
+let idade = prompt("Qual a sua idade?");
+alert("Você tem " + idade);
+let pais = prompt("Qual pais você mora?");
+alert("Que legal você mora no " + pais);
+alert("Seja muito bem vindo a nossa página!")
