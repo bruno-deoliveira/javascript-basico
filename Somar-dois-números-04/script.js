cfff08d908bdb224e9d49472406e8b69ff5ff72e
Bruno Oliveira <109918729+bruno-deoliveira@users.noreply.github.com>
2023-06-27 21:36:18 -0300

    Add files via upload

---
+++ Somar-dois-números-04/script.js
@@ -0,0 +1,13 @@
+//Somar dois números
+
+//Solicite 2 números, faça a soma deles e apresente o resultado final ao usuário
+//1. modelo:
+alert('Iremos somar 2 números');
+let numberOne = prompt('Digite o primeiro númro: ');
+let numberTwo = prompt('Digite o segundo número: ');
+
+alert ("Resultado final " +((Number(numberOne) + Number(numberTwo))))
+
+alert (Number(numberOne) + Number(numberTwo))
+
+alert ((Number(numberOne) + Number(numberTwo)) / 2)
---
+++ Somar-dois-números-04/script2.js
@@ -0,0 +1,6 @@
+//2. modelo
+alert('Iremos somar 2 números');
+let numberOne = prompt('Digite o primeiro númro: ');
+let numberTwo = prompt('Digite o segundo número: ');
+let result = Number(numberOne) + Number(numberTwo)
+alert ("Resultado final é " + result)
